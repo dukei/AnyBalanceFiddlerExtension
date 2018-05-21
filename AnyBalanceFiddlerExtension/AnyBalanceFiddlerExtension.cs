c27265bdeb260639d96c469e6612f4a8c5ee2118
Dmitry Kochin <dco@mail.ru>
2018-05-21 15:29:45 +0300

    Fixed handling redirects to the same protocol but another domain (like "//domain/path")

--- AnyBalanceFiddlerExtension/AnyBalanceFiddlerExtension.cs
+++ AnyBalanceFiddlerExtension/AnyBalanceFiddlerExtension.cs
@@ -24,25 +24,37 @@ public class AnyBalanceExtension : IAutoTamper2
         {
             int[] redirectCodes = { 301, 302, 303, 307 };
 
-            if (Array.IndexOf(redirectCodes, oSession.responseCode) >= 0){
+            if (Array.IndexOf(redirectCodes, oSession.responseCode) >= 0)
+            {
                 String loc = oSession.oResponse.headers["Location"];
-                if (Regex.IsMatch(loc, "^https?://", RegexOptions.IgnoreCase)){
-                    Regex re = new Regex("^(https?://[^/]+)", RegexOptions.IgnoreCase);
-                    Match matchesDomain = re.Match(loc);
-                    Match matchesUrl = re.Match(oSession.fullUrl);
-                    String locDomain = matchesDomain.Success ? matchesDomain.Groups[1].Value : null;
-                    String urlDomain = matchesUrl.Success ? matchesUrl.Groups[1].Value : null;
-                    if (locDomain != null && urlDomain != null && locDomain.ToLower() != urlDomain.ToLower())
+                Regex re = new Regex("^(https?:)?//([^/]+)(.*)", RegexOptions.IgnoreCase);
+                Match urlMatch = re.Match(oSession.fullUrl);
+                Match locMatch = re.Match(loc);
+                if (urlMatch.Success && locMatch.Success)
+                {
+                    String urlSchema = urlMatch.Groups[1].Value;
+                    String urlDomain = urlMatch.Groups[2].Value;
+                    String locSchema = locMatch.Groups[1].Value;
+                    String locDomain = locMatch.Groups[2].Value;
+                    if (String.IsNullOrEmpty(locSchema))
+                        locSchema = urlSchema;
+
+                    if (locDomain.ToLower() != urlDomain.ToLower() || locSchema.ToLower() != urlSchema.ToLower())
                     {
                         oSession.responseCode += 400;
+                        //Rewrite location to include schema explicitly
+                        oSession.oResponse["Location"] = locSchema + "//" + locDomain + locMatch.Groups[3].Value;
                         FiddlerApplication.Log.LogString("AnyBalance: Fiddling redirect from " + urlDomain + " to " + loc + " with code " + oSession.responseCode);
                     }
+                } else if(!urlMatch.Success)
+                {
+                    FiddlerApplication.Log.LogString("AnyBalance: Could not extract schema and domain from  " + oSession.fullUrl);
+                }
             }
         }
     }
-}
 
-public void AutoTamperRequestAfter(Session oSession) { }
+    public void AutoTamperRequestAfter(Session oSession) { }
     public void AutoTamperResponseAfter(Session oSession) { }
     public void AutoTamperResponseBefore(Session oSession) { }
     public void OnBeforeReturningError(Session oSession) { }
